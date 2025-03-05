31741712198b6b51a2205f8f804574d67c1c09e7
dieguga07 <dieguga04@gmail.com>
2025-03-05 10:57:46 +0100

    point and click

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,14 +14,29 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-            // Verificar si el agente ha llegado al destino
-        if (!agent.pathPending && agent.remainingDistance < 1.5f)
-        {
-            // Avanzar al siguiente punto
-            currentPointIndex = (currentPointIndex + 1) % points.Length;
 
-            // Establecer el destino al siguiente punto
-            agent.SetDestination(points[currentPointIndex].position);
+        // Verificar si el agente ha llegado al destino
+        //if (!agent.pathPending && agent.remainingDistance < 1.5f)
+
+            if (Input.GetMouseButtonDown(0))  // 0 es el botón izquierdo del ratón
+        {
+
+
+            //currentPointIndex = (currentPointIndex + 1) % points.Length;
+
+            //agent.SetDestination(points[currentPointIndex].position);
+
+            // Hacer un rayo desde la cámara hacia el punto donde se hizo clic
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Si el rayo toca el terreno u objeto con un collider (lo que sea en el escenario)
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Establecer la nueva posición de destino para el agente de navegación
+                agent.SetDestination(hit.point);
+            }
         }
     }
 }
+
